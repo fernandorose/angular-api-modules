627e0463fe847db393dd98437ba8594f941e8dd9
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-05-07 09:37:10 -0600

    Fix duplicate comment for geist-mono font import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app/app.component';
 
 // Supports weights 100-900
 import '@fontsource-variable/inter';
+// Supports weights 100-900
+import '@fontsource-variable/geist-mono';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
