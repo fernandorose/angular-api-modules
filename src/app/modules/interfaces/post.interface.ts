e8aef027f55b62400d8d79cb74afd931aca515e5
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-05-08 08:30:20 -0600

    Add Post interface to define structure for blog posts

---
+++ src/app/modules/interfaces/post.interface.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  tags: string[];
+  reactions: {
+    likes: number;
+    dislikes: number;
+  };
+  views: number;
+}
